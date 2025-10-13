6dbf7057180189022cc98a48b97752207a1e8c48
imaddde867 <63160938+imaddde867@users.noreply.github.com>
2025-10-13 14:44:47 +0300

    Add Deno configuration and initial HTML template

--- server/app.js
+++ server/app.js
@@ -1,18 +1,35 @@
-import { Hono } from "@hono/hono";
-import { cors } from "@hono/hono/cors";
-import { logger } from "@hono/hono/logger";
-import postgres from "postgres";
+import { Hono } from "jsr:@hono/hono@4.6.5";
 
 const app = new Hono();
-const sql = postgres();
 
-app.use("/*", cors());
-app.use("/*", logger());
-
-app.get("/", (c) => c.json({ message: "Hello world!" }));
-app.get("/todos", async (c) => {
-  const todos = await sql`SELECT * FROM todos`;
-  return c.json(todos);
+app.get("/", async (c) => {
+  return c.html(`<!DOCTYPE html>
+<html>
+  <head>
+    <title>Title</title>
+  </head>
+  <body>
+    <h1>Details</h1>
+    <form method="POST" action="/details">
+      <label for="email">Email:</label>
+      <input type="email" name="email" id="email" />
+      <br />
+      <label for="password">Password:</label>
+      <input type="password" name="password" id="password" />
+      <br />
+      <label for="nickname">Nickname:</label>
+      <input type="text" name="nickname" id="nickname" />
+      <br />
+      <label for="yearOfBirth">Year of birth:</label>
+      <input type="number" name="yearOfBirth" id="yearOfBirth" />
+      <br />
+      <label for="color">Favorite color:</label>
+      <input type="color" name="color" id="color" />
+      <br />
+      <input type="submit" value="Submit details" />
+    </form>
+  </body>
+</html>`);
 });
 
-export default app;
+Deno.serve(app.fetch);
